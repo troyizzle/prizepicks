e1d2c0a91f136c7ceaaad7489145c015e804cfce
Troy Underwood <troyizzle@gmail.com>
2023-03-02 09:48:22 -0600

    feat: client inspect filter
    Currently inspecting the client would show the password, this is not a
    good idea.
    
    This feature should filter the password on inspect
    
    Closes #5

--- test/prizepicks/test_client.rb
+++ test/prizepicks/test_client.rb
@@ -3,22 +3,15 @@ require 'test_helper'
 
 module PrizePicks
   class TestClient < MiniTest::Test
-    # def test_client_setup_raises_error_without_email
-    #   assert_raises ArgumentError do
-    #     init_client(password: 'foobar')
-    #   end
-    # end
-    #
-    # def test_client_setup_raises_error_without_password
-    #   assert_raises ArgumentError do
-    #     init_client(email: 'foo@foo.com')
-    #   end
-    # end
-
     def test_email
       client = init_client(email: 'foo@foo.com', password: 'foobar')
       assert_equal 'foo@foo.com', client.email
       assert_equal 'foobar', client.password
+    end
+
+    def test_inpsect
+      client = init_client(email: 'foo@foo.com', password: 'foobar')
+      assert_match(/[FILTERED]/, client.inspect)
     end
   end
 end
